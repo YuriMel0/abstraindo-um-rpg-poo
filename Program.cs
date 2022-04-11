09b443e8626a15405510757245b3a9f61f7a58b2
Yuri Mel0 <meloy316@gmail.com>
2022-04-11 11:08:18 -0300

    criando arquivos para o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abstraindo_um_rpg_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
